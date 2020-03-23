0f2cb2f662966c291aef1c8ce00e6161208568e5
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-03-23 18:12:46 +0100

    first commit

---
+++ src/main/java/com/frontend/gamesjar/GamesjarApplication.java
@@ -0,0 +1,13 @@
+package com.frontend.gamesjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesjarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesjarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frontend/gamesjar/GamesjarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend.gamesjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesjarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
